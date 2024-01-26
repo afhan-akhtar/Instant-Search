35e2ae554e98261de3ddb36ba25a328a1a93fdc3
afhan-akhtar <affanlodhi333@gmail.com>
2024-01-26 11:06:49 +0500

    instant-search vue-app

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <InstantSearch />
+  </div>
+</template>
+
+<script>
+import InstantSearch from './components/InstantSearch.vue'
+
+export default {
+  name: 'App',
+  components: {
+    InstantSearch
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.centered-list {
+  list-style: none;
+  padding: 0;
+  text-align: center;
+}
+
+.centered-list li {
+  margin: 10px 0;
+  /* Adjust the margin as needed */
+}
+</style>
+
---
+++ src/components/InstantSearch.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Instant Search App</h1>
+        <input type="text" v-model="searchTerm" placeholder="Search..." />
+        <ul class="centered-list">
+            <li v-for="result in filteredResults" :key="result.id">{{ result.title }}</li>
+        </ul>
+    </div>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+            searchTerm: "",
+            results: [
+                { id: 1, title: "Apple" },
+                { id: 2, title: "Blackberry" },
+                { id: 3, title: "Computer" },
+                { id: 4, title: "Date" },
+                { id: 5, title: "England" },
+            ],
+        };
+    },
+    computed: {
+        filteredResults() {
+            return this.results.filter((result) =>
+                result.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+            );
+        },
+    },
+};
+</script>
